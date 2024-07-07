13d77c54cf35f8d707b3c13722469e9eb087c523
Nikita Krenov <nikitabaram@gmail.com>
2024-07-07 12:32:35 +0300

    study time

--- main.cpp
+++ main.cpp
@@ -3,8 +3,51 @@
 
 using namespace std;
 
+struct Console {
+	static void showError(string message) {
+		cout << message << endl;
+	}
+};
+
+struct CustomColor {
+private:
+    string hex;
+    static int objectCount;
+
+public:
+    CustomColor(string hex) : hex(hex) {
+        objectCount++;
+    }
+
+    static int getObjectCount() {
+        return objectCount;
+    }
+
+    string getHex() const {
+        return hex;
+    }
+
+    ~CustomColor() {
+        objectCount--;
+    }
+};
+
+int CustomColor::objectCount = 0;
+
 int main() {
+    CustomColor* color_1 = new CustomColor("FF0000");
+    cout << "Object count = " << CustomColor::getObjectCount() << endl;
+
+    CustomColor* color_2 = new CustomColor("00FF00");
+
+    cout << "Color 1 = " << color_1->getHex() << endl;
+    cout << "Color 2 = " << color_2->getHex() << endl;
+
+    delete color_1;
+    delete color_2;
+
+    cout << "Object count after deletion = " << CustomColor::getObjectCount() << endl;
 
 	system("pause");
-	return 0;
+    return 0;
 }
